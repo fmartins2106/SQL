6a1abd1da37d2eb13874de2105ac83b28fbfd73c
Fernando Martins <fernandom.adm@gmail.com>
2025-07-05 23:35:15 -0300

    Adicionando Exemplos UNION, UNIONALL

---
+++ postgre/UNION,UNIONALL.sql
@@ -0,0 +1,30 @@
+SELECT 'DEVDOJO' AS nome,
+       'JAVA' AS linguagem
+UNION
+SELECT 'Google' AS nome,
+       'Kotlin' AS linguagem
+UNION ALL
+SELECT 'DEVDOJO' AS nome,
+       'JAVA' AS linguagem;
+
+
+select * from empresa.funcionarios limit 3
+
+SELECT * FROM empresa.clientes
+
+select * from empresa.cargos limit 4
+
+
+SELECT
+nome,
+sobrenome,
+data_nascimento,
+email
+from empresa.funcionarios
+UNION all
+SELECT
+nome,
+endereco,
+data_cadastro,
+email
+from empresa.clientes
