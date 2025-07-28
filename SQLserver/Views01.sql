b8f26cffe0bded85138db2ce9238e67d1d0465a1
Fernando Martins <fernandom.adm@gmail.com>
2025-07-28 02:07:37 -0300

    Tabela temporaria e VIEWS

---
+++ SQLserver/Views01.sql
@@ -0,0 +1,8 @@
+-- auto-generated definition
+create schema HumanResources
+go
+
+exec sp_addextendedproperty 'MS_Description', 'Contains objects related to employees and departments.', 'SCHEMA',
+     'HumanResources'
+go
+
