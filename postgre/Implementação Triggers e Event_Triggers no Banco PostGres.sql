71b9759800adff33d25fb4c8c60d9842b670d90b
Fernando Martins <fernandom.adm@gmail.com>
2025-08-29 02:10:52 -0300

    Adicionando triggers - tabela postgres-DevDojo

--- postgre/Implementação Triggers e Event_Triggers no Banco PostGres.sql
+++ postgre/Implementação Triggers e Event_Triggers no Banco PostGres.sql
@@ -51,8 +51,8 @@ $$
 BEGIN
     IF new.salario <= 1510 THEN
         RAISE EXCEPTION 'Salário não pode ser menor que salário mínimo.';
-        RETURN old;
     END IF;
+    RETURN new;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -84,5 +84,232 @@ CREATE OR REPLACE TRIGGER tgr_validacao_data_demissao
 EXECUTE FUNCTION fn_validacao_data_demissao();
 
 
+--1. Garantir salário >= 1510
+
+CREATE OR REPLACE FUNCTION fn_validando_salario_minimo()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    IF new.salario < 1510 THEN
+        RAISE EXCEPTION 'Salário não pode ser menor que salário mínimo';
+    END IF;
+    RETURN new;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER tgr_validando_salario_minimo
+    BEFORE UPDATE OF salario OR INSERT
+    ON db_funcionario
+    FOR EACH ROW
+EXECUTE FUNCTION fn_validando_salario_minimo();
+
+-- 2. Data de demissão >= admissão
+CREATE OR REPLACE FUNCTION fn_validando_data_demissao()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    IF new.data_demissao < old.data_admissao OR new.data_demissao > CURRENT_DATE THEN
+        RAISE EXCEPTION 'Erro. data de demissão não pode ser menor que data de admissão ou maior que data atual.';
+    ELSE
+        new.ativo = FALSE;
+    END IF;
+    RETURN new;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER tgr_validando_data_demissao
+    BEFORE UPDATE OF data_demissao
+    ON db_funcionario
+    FOR EACH ROW
+EXECUTE FUNCTION fn_validando_data_demissao();
+
+-- 3. Log exclusão de cliente
+
+CREATE TABLE administration.log_exclusao_cliente
+(
+    id_log_cliente INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id_cliente     INTEGER      NOT NULL,
+    nome_cliente   VARCHAR(100) NOT NULL,
+    usuario        VARCHAR(50)  NOT NULL DEFAULT CURRENT_USER,
+    data_exclusao  TIMESTAMP    NOT NULL DEFAULT NOW(),
+    CONSTRAINT FK_id_cliente_log_exclusao FOREIGN KEY (id_cliente) REFERENCES db_cliente (id_cliente)
+);
+
+
+CREATE OR REPLACE FUNCTION fn_log_exclusao_cliente()
+    RETURNS TRIGGER AS
+$$
+DECLARE
+    v_nome_cliente TEXT;
+BEGIN
+
+    IF old.ativo = TRUE AND new.ativo = FALSE THEN
+
+        SELECT p.nome
+        INTO v_nome_cliente
+        FROM db_pessoa p
+                 INNER JOIN db_cliente f
+                            ON f.id_pessoa = p.id_pessoa
+        WHERE old.id_cliente = f.id_cliente;
+
+        INSERT INTO administration.log_exclusao_cliente(id_cliente, nome_cliente)
+        VALUES (old.id_cliente, v_nome_cliente);
+    END IF;
+    RETURN new;
+
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER tgr_log_exclusao_cliente
+    AFTER UPDATE OF ativo
+    ON db_cliente
+    FOR EACH ROW
+EXECUTE FUNCTION fn_log_exclusao_cliente();
+
+UPDATE db_cliente
+SET ativo = FALSE
+WHERE id_cliente = 105;
+
+SELECT *
+FROM administration.log_exclusao_cliente;
+
+
+DROP FUNCTION fn_log_cliente_inativo() CASCADE;
+SELECT *
+FROM db_cliente
+LIMIT 3;
+
+
+-- 4. Bloquear exclusão de vendas
+
+SELECT *
+FROM db_venda
+LIMIT 3;
+
+CREATE OR REPLACE FUNCTION fn_bloquear_exclusao_venda()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    RAISE EXCEPTION 'Proibido excluir dados da venda.';
+    RETURN old;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER tgr_bloquear_exclusao_venda
+    BEFORE DELETE
+    ON db_venda
+    FOR EACH ROW
+EXECUTE FUNCTION fn_bloquear_exclusao_venda();
+
+
+-- 5. valor_produto_menor_que_zero
+
+CREATE OR REPLACE FUNCTION fn_valor_produto_menor_zero()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    IF new.preco < 0 THEN
+        RAISE EXCEPTION 'Erro, valor do produto não pode ser menor que zero.';
+    END IF;
+    RETURN new;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER tgr_valor_produto_menor_zero
+    BEFORE INSERT OR UPDATE OF preco
+    ON db_produto
+    FOR EACH ROW
+EXECUTE FUNCTION fn_valor_produto_menor_zero();
+
+
+SELECT *
+FROM db_produto
+LIMIT 3;
+
+
+-- 6. valor_venda_menor_que_zero
+
+CREATE OR REPLACE FUNCTION fn_valor_total_venda_menor_zero()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    IF new.total < 0 THEN
+        RAISE EXCEPTION 'Erro, valor do produto não pode ser menor que zero.';
+    END IF;
+    RETURN new;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER tgr_valor_produto_menor_zero
+    BEFORE INSERT OR UPDATE OF total
+    ON db_venda
+    FOR EACH ROW
+EXECUTE FUNCTION fn_valor_total_venda_menor_zero();
+
+
+-- 7. Log de alterações salariais
+
+CREATE TABLE administration.log_alteracao_salarial
+(
+    id_log_alteracao_salario INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    matricula                INTEGER       NOT NULL,
+    salario_antigo           NUMERIC(7, 2) NOT NULL,
+    novo_salario             NUMERIC(7, 2) NOT NULL,
+    usuario                  VARCHAR(30)   NOT NULL DEFAULT CURRENT_USER,
+    data_alteracao           TIMESTAMP     NOT NULL DEFAULT NOW(),
+    CONSTRAINT FK_log_alteracao_salarioal FOREIGN KEY (matricula) REFERENCES db_funcionario (matricula)
+);
+
+CREATE OR REPLACE FUNCTION fn_log_alteracao_salarial()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    IF new.salario <> old.salario or old.salario IS NULL THEN
+        IF (tg_op = 'INSERT') THEN
+            INSERT INTO administration.log_alteracao_salarial(matricula, salario_antigo, novo_salario)
+            VALUES (new.matricula, 0, new.salario);
+        END IF;
+
+        IF (tg_op = 'UPDATE') THEN
+            INSERT INTO administration.log_alteracao_salarial(matricula, salario_antigo, novo_salario)
+            VALUES (old.matricula, old.salario, new.salario);
+        END IF;
+    END IF;
+    RETURN new;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER tgr_log_alteracao_salarial
+    AFTER UPDATE OF salario OR INSERT
+    ON db_funcionario
+    FOR EACH ROW
+EXECUTE FUNCTION fn_log_alteracao_salarial();
+
+
+SELECT * FROM db_funcionario order by matricula desc;
+
+select * from administration.log_alteracao_salarial;
+
+update db_funcionario SET salario = 1999.99 where matricula = 1035;
+
+DROP TRIGGER tgr_log_demissao ON db_funcionario;
+
+INSERT INTO DB_FUNCIONARIO
+(id_pessoa, data_admissao, data_demissao, salario, id_cargo, id_departamento, id_nivel_funcionario)
+VALUES
+(44, '2020-01-15', NULL, 2500.00, 1, 1, 3);
+
+
+CREATE ROLE usuario_test WITH LOGIN PASSWORD '123123';
+CREATE ROLE usuario_test WITH LOGIN SUPERUSER PASSWORD '123123';
+REVOKE ALL PRIVILEGES ON DATABASE postgres from usuario_test;
+REVOKE ALL PRIVILEGES on SCHEMA public from usuario_test;
+REVOKE ALL PRIVILEGES ON SCHEMA administration from usuario_test;
+ALTER ROLE usuario_test NOINHERIT NOCREATEDB NOSUPERUSER NOSUPERUSER ;
+GRANT CONNECT ON DATABASE postgres TO usuario_test;
+GRANT USAGE ON SCHEMA public TO usuario_test;
+GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA public TO usuario_test;
 
 
